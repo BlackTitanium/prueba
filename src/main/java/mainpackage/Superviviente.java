923bf9eeda4d6314afe4c0b6f7ba5b64fe0ce64b
albiol2004 <albiol2004@gmail.com>
2024-12-28 18:35:04 +0100

    modified:   src/main/java/mainpackage/Superviviente.java 	modified:   target/classes/mainpackage/Superviviente$estado.class 	modified:   target/classes/mainpackage/Superviviente.class

--- src/main/java/mainpackage/Superviviente.java
+++ src/main/java/mainpackage/Superviviente.java
@@ -4,12 +4,42 @@ public class Superviviente extends Entidad{
     private String nombre;
     private int contadorZombis, mordeduras, acciones = 3;
     private accion seleccion;
-    private Arma arma;
+    private Arma[] arma = new Arma[2];
 
+    public int getContadorZombis() {
+        return contadorZombis;
+    }
+
+    public int getMordeduras() {
+        return mordeduras;
+    }
+
+    public int getAcciones() {
+        return acciones;
+    }
+
+    public accion getSeleccion() {
+        return seleccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Arma[] getArma() {
+        return arma;
+    }
    
     private enum estado {VIVO, MUERTO};
     private estado estadoActual;
-    private Equipo[] inventario; 
+    private Equipo[] inventario = new Equipo[5]; 
+
+    public estado getEstadoActual() {
+        return estadoActual;
+    }
+    public Equipo[] getInventario() {
+        return inventario;
+    }
 
     public Superviviente(String nombre){
         this.nombre = nombre;
@@ -20,6 +50,11 @@ public class Superviviente extends Entidad{
 
     public void addMordeduras(){
         mordeduras++;
+    }
+
+    public void buscar(int a){
+        inventario[a] = casillaActual.buscar();
+        acciones--;
     }
 
     @Override
@@ -40,19 +75,34 @@ public class Superviviente extends Entidad{
         }
     }
 
-    public void elegirArma(int a){
+    public void elegirArma(int a, int b){
         if(inventario[a] instanceof Arma){
             Equipo equipoSeleccionado = inventario[a];
-            arma = (Arma) equipoSeleccionado;
+            arma[b] = (Arma) equipoSeleccionado;
+        } else {
+            throw new IllegalArgumentException("No es un arma");
         }
     }
+
+    public Provision elegirProvision(int a){
+        if(inventario[a] instanceof Provision){
+            Equipo equipoSeleccionado = inventario[a];
+            Provision provision = (Provision) equipoSeleccionado;
+            acciones--;
+            return provision;
+        } else {
+            throw new IllegalArgumentException("No es una provision");
+        }
+    }
+
     public Zombi elegirObjetivo(Zombi[] zombis, int a){
         return zombis[a];
     }
 
-    public Ataque atacar() {
-            return new Ataque(arma);
+    @Override
+    
+    public Ataque atacar(int a) {
+            return new Ataque(arma[a]);
     }
     
-
 }
