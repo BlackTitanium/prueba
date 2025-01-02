2e1c5d50b088fe4e094ea0547d530e855ad29711
Monthy765 <maroa.nr.2@gmail.com>
2025-01-02 10:52:05 +0100

    modified:   src/main/java/mainpackage/Partida.java 	modified:   target/classes/mainpackage/Partida.class

--- src/main/java/mainpackage/Partida.java
+++ src/main/java/mainpackage/Partida.java
@@ -1,10 +1,6 @@
 package mainpackage;
 
 import java.util.ArrayList;
-import javax.swing.*;
-
-import mainpackage.Interfaz.*;
-import static mainpackage.Interfaz.InterfazPrincipal.numJ;
 
 public class Partida {
     public ArrayList<Superviviente> supervivientes;
@@ -33,32 +29,7 @@ public class Partida {
         turnoActual = (turnoActual + 1) % supervivientes.size();
     }
     
-    private int jugadoresAñadidos = 0;
-    
-    public void añadirSuperviviente(JFrame parent){
-        if(jugadoresAñadidos < InterfazInicio.nJugadores){
-            numJ = jugadoresAñadidos+1;
-//            InterfazNombreJugador dialog = new InterfazNombreJugador();
-//            dialog.setModal(true); // Bloquea el bucle mientras InterfazNombreJugador este abierto
-//            dialog.setVisible(true);
-            InterfazNombreJugador dialog = new InterfazNombreJugador(parent);
-            dialog.mostrar();
-
-            if(!InterfazNombreJugador.nombre.isEmpty()){
-                Superviviente s = new Superviviente(InterfazNombreJugador.nombre);
-                InterfazPrincipal.casillas[0][0].addSuperviviente(s);
-                InterfazPrincipal.botones[0][0].setText("S" + (jugadoresAñadidos+1));  // Mostrar "S" + (jugadoresAñadidos+1) en el botón
-                addSuperviviente(s);
-                
-                jugadoresAñadidos++;
-            } else {
-                JOptionPane.showMessageDialog(null,"Debes ingresar un nombreeeee.");
-            }
-            añadirSuperviviente(parent);
-        }
-    }
-    
     public void main(String args[]){
-        new InterfazInicio().setVisible(true);
+        
     }
 }
